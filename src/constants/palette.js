53f0f1c39ec765d71e648b4307b453b65f627924
maurisilvera <129180621+maurisilvera@users.noreply.github.com>
2023-04-14 11:28:46 -0300

    Initial commit

---
+++ src/constants/palette.js
@@ -0,0 +1,83 @@
+export const PALETTE_MAPPER = {
+  Palette: {
+    dark: {
+      '01': 'var(--dark01)',
+      '02': 'var(--dark02)',
+      '03': 'var(--dark03)',
+      '04': 'var(--dark04)',
+      '05': 'var(--dark05)'
+    },
+    gray: {
+      '01': 'var(--gray01)',
+      '02': 'var(--gray02)',
+      '03': 'var(--gray03)',
+      '04': 'var(--gray04)',
+      '05': 'var(--gray05)'
+    },
+    light: {
+      '01': 'var(--light01)',
+      '02': 'var(--light02)',
+      '03': 'var(--light03)',
+      '04': 'var(--light04)',
+      '05': 'var(--light05)'
+    },
+    success: {
+      '01': 'var(--semanticSuccess01)',
+      '02': 'var(--semanticSuccess02)',
+      '03': 'var(--semanticSuccess03)',
+      '04': 'var(--semanticSuccess04)',
+      '05': 'var(--semanticSuccess05)'
+    },
+    error: {
+      '01': 'var(--semanticError01)',
+      '02': 'var(--semanticError02)',
+      '03': 'var(--semanticError03)',
+      '04': 'var(--semanticError04)',
+      '05': 'var(--semanticError05)'
+    },
+    warning: {
+      '01': 'var(--semanticWarning01)',
+      '02': 'var(--semanticWarning02)',
+      '03': 'var(--semanticWarning03)',
+      '04': 'var(--semanticWarning04)',
+      '05': 'var(--semanticWarning05)'
+    },
+    information: {
+      '01': 'var(--semanticInformation01)',
+      '02': 'var(--semanticInformation02)',
+      '03': 'var(--semanticInformation03)',
+      '04': 'var(--semanticInformation04)',
+      '05': 'var(--semanticInformation05)'
+    },
+    actions: {
+      accent: {
+        '01': 'var(--actionAccent01)',
+        '02': 'var(--actionAccent02)',
+        '03': 'var(--actionAccent03)',
+        '04': 'var(--actionAccent04)',
+        '05': 'var(--actionAccent05)'
+      },
+      neutral: {
+        '01': 'var(--actionNeutral01)',
+        '02': 'var(--actionNeutral02)',
+        '03': 'var(--actionNeutral03)',
+        '04': 'var(--actionNeutral04)',
+        '05': 'var(--actionNeutral05)'
+      },
+      error: {
+        '01': 'var(--actionError01)',
+        '02': 'var(--actionError02)',
+        '03': 'var(--actionError03)',
+        '04': 'var(--actionError04)',
+        '05': 'var(--actionError05)'
+      },
+      negative: {
+        '01': 'var(--actionNegative01)',
+        '02': 'var(--actionNegative02)',
+        '03': 'var(--actionNegative03)',
+        '04': 'var(--actionNegative04)',
+        '05': 'var(--actionNegative05)'
+      }
+    }
+  }
+};
