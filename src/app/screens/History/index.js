3d9f51b7d4b20de36122844e73657941c5f45f57
Mauricio Silvera <mauricio.silvera@widergy.com>
2023-04-20 13:17:43 -0300

    PR suggestions

--- src/app/screens/History/index.js
+++ src/app/screens/History/index.js
@@ -4,21 +4,19 @@ import { connect } from 'react-redux';
 
 import styles from './styles.module.scss';
 
-function History({ history }) {
-  return (
-    <div className={styles.historyContainer}>
-      <h2>Historial</h2>
-      <ul className={styles.historyList}>
-        {history.map(item => (
-          <li className={styles.historyCard}>
-            Fecha: {item.date} <br />
-            Resultado: {item.value}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+const History = ({ history }) => (
+  <div className={styles.historyContainer}>
+    <h2>Historial</h2>
+    <ul className={styles.historyList}>
+      {history.map(item => (
+        <li className={styles.historyCard}>
+          Fecha: {item.date} <br />
+          Resultado: {item.value}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 History.propTypes = {
   history: PropTypes.arrayOf.isRequired
